561513f2149d617f7c87c02b14d1220ce3e5b4ce
a <a@q.com>
2024-01-15 19:42:35 +0800

    update

--- installCFile.sh
+++ installCFile.sh
@@ -1,2 +1,4 @@
+sudo rm -rf /usr/local/include/frida-core.h
+sudo rm -rf /usr/local/include/libfrida-core.a
 sudo cp ./cfile/frida-core.h /usr/local/include/
 sudo cp ./cfile/libfrida-core.a /usr/local/lib
